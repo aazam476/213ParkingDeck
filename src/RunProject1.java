361cb5deb174969a9d91c0bcef59ed8a28b9ebf8
Ali Azam <aliaccounts@azam.email>
2026-09-17 01:13:00 +0000

    feat: create project & add driver class create the java project with initial boilerplate code and add the RunProject 1 driver class

---
+++ src/RunProject1.java
@@ -0,0 +1,20 @@
+/**
+ * The driver class to execute Project 1.
+ * This class contains the main method which instantiates the
+ * {@code Operation} controller and hands off execution control by
+ * calling its {@code run()} method.
+ *
+ * @author Ali Azam
+ */
+
+public class RunProject1 {
+
+    /**
+     * The main entry point for the application.
+     *
+     * @param args unused cmd args
+     */
+    public static void main(String[] args) {
+        new Operation().run();
+    }
+}
